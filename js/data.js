96df148e740f8d7145a57b03a8a232e5c8ca21fc
Bahrom <113708211+NabiyevBahrom@users.noreply.github.com>
2023-06-12 13:18:09 +0500

    Javascript template

---
+++ js/data.js
@@ -0,0 +1,18 @@
+const navbar = {
+    home: "Home",
+    about: "About",
+    contact: "Contact",
+    projects: "Project",
+}
+// base
+const base = {
+    hello: "Hello",
+    name: "I am Nabiyev Bahrom",
+    front: "Junior Frontend Designer",
+    number: "+998 (94) 706-66-37",
+    work: "0",
+    year: "Years",
+};
+const img = { 
+    banner: "../img/home-right.jpg",
+}
